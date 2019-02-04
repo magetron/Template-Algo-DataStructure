b3628eec1f5db3bfe95f88eefce6c9f969c536aa
Patrick Wu <magetron1@gmail.com>
2019-02-04 22:23:53 +0000

    finished binaryheap

--- binaryheap.c
+++ binaryheap.c
@@ -1,12 +1,6 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-// max binary heap
-//		16
-// 	14		11
-// 5	     3	     7	 	4
-
-
 void swap (int *a, int *b) {
 	int t = *a;
 	*a = *b;
@@ -15,15 +9,25 @@ void swap (int *a, int *b) {
 
 void swim (int *arr, int loc, int size) {
 	while (loc > 0) {
-		if (arr[loc / 2] < arr[loc]) swap(&arr[loc / 2], &arr[loc]);
-		else break;
+		if (arr[loc / 2] < arr[loc]) {
+			swap(&arr[loc / 2], &arr[loc]);
+			loc = loc / 2;
+		} else break;
 	}
 }
 
 void sink (int *arr, int loc, int size) {
-	while (loc < size - 1) {
-		if (arr[loc * 2] > arr[loc]) swap(&arr[loc * 2], &arr[loc]);
-		else if (arr[loc * 2 + 1] > arr[loc]) swap(&arr[loc * 2 + 1], &arr[loc]);
+	while (loc <=  (size - 1) / 2) {
+		int greater = loc * 2 + 1;
+		if (arr[loc * 2] > arr[loc * 2 + 1]) greater = loc * 2;
+		if (arr[greater] > arr[loc]) {
+			swap(&arr[greater], &arr[loc]);
+			loc = greater;
+		}
+		else if (arr[greater] > arr[loc]) {
+			swap(&arr[greater], &arr[loc]);
+			loc = greater;
+		}
 		else break;
 	}
 }
@@ -39,7 +43,9 @@ int *init_heap (int size) {
 }
 
 int main () {
+	//freopen("binaryheap.in", "r", stdin); 
 	int max_size;
+	printf("Max Heap Size = ");
 	scanf("%d", &max_size);
 	int *arr = init_heap(max_size);
 	int size = 0;
@@ -53,24 +59,28 @@ int main () {
 		int new_element;
 		switch(op) {
 			case 1 : scanf("%d", &new_element);
-				 if (++size > max_size) {
+				 if (size + 1 > max_size) {
 					 printf("ERROR : Segmentfault\n");
 					 break;
 				 } else {
+					 size++;
 					 arr[size - 1] = new_element;
 					 swim(arr, size - 1, size);
 				 }
-				 printf("%d\n", size);
 				 break;
-			case 2 : printf("pop = %d\n", arr[0]);
-				 size--;
-				 arr[0] = arr[size];
-				 sink(arr, 0, size);
+			case 2 : if (size - 1 < 0) printf("ERROR : Segmentfault\n");
+				 else {
+					printf("pop = %d\n", arr[0]);
+				 	arr[0] = arr[size - 1];
+				 	size--;
+				 	sink(arr, 0, size);
+				 }
 				 break;
 			case 3 : print_heap(arr, size);
 				 break;
 		}
 	}
+	//fclose(stdin);
 	return 0;
 }
 
