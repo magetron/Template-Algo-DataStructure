f8797ba4b632356e8954c9ad795dd6be844adc99
Patrick Wu <magetron1@gmail.com>
2019-08-17 18:05:29 +0100

    finish pqueue heap

--- priority-queue.c
+++ priority-queue.c
@@ -20,7 +20,7 @@ void swim (int *heap, int loc, int size) {
 
 void sink (int *heap, int loc, int size) {
 	int cur = loc;
-	while (heap[cur] < heap[cur * 2] || heap[cur] < heap[cur * 2 + 1]) 
+	while ((heap[cur] < heap[cur * 2] && cur * 2 <= size) || (heap[cur] < heap[cur * 2 + 1] && cur * 2 + 1 <= size)) 
 		if (heap[cur] < heap[cur * 2]) {
 			swap(&heap[cur], &heap[cur * 2]);
 			cur *= 2;
@@ -39,12 +39,9 @@ int pop (int *heap, int *size) {
 	int num = heap[1];
 	swap(&heap[*size], &heap[1]);
 	*size = *size - 1;
-	printf("size=%d\n", *size);
 	sink(heap, 1, *size);
 	return num;
 }
-	
-
 
 int main () {
 	int heap[MAX_HEAP_SIZE];
@@ -56,9 +53,10 @@ int main () {
 	add(heap, 17, &size);
 	add(heap, 3, &size);
 	add(heap, 11, &size);
-	for (int i = 1; i <= size; i++) printf("%d ", heap[i]);
-	printf("\n%d %d\n", pop(heap, &size), 1)//pop(heap, &size));
-	for (int i = 1; i <= size; i++) printf("%d ", heap[i]);
+	for (int i = 1; i <= size; i++) printf("%d ", heap[i]); printf("\n");
+	printf("%d %d\n", pop(heap, &size), pop(heap, &size));
+	for (int i = 1; i <= size; i++) printf("%d ", heap[i]); printf("\n");
+	printf("%d %d %d\n", pop(heap, &size), pop(heap, &size), pop(heap, &size));
 	return 0;
 }
 
