1c188f8e50fbe3d9f06febcc71b7ea2af6501c1f
Patrick Wu <magetron1@gmail.com>
2019-09-06 14:51:11 +0100

    add unordered set vector int template

---
+++ unordered_set<vector<int>>.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct VectorHash {
+	size_t operator()(const std::vector<int>& v) const {
+		std::hash<int> hasher;
+		size_t seed = 0;
+		for (int i : v) 
+			seed ^= hasher(i) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+        return seed;
+	}
+};	
+
+using my_set = unordered_set<vector<int>, VectorHash>;
+
