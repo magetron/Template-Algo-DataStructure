0c7a3261df2cbd1730950d5c91e4f139fef30d6f
Patrick Wu <magetron1@gmail.com>
2020-04-07 17:04:26 +0100

    add hello world

---
+++ hello-world.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#pragma GCC optimize ("Ofast")
+
+using namespace std;
+
+static int fast_io = [] () {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	return 0;
+} ();
+
+int main () {
+	cout << "Hello World!" << endl;
+	return 0;
+}
